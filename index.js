09ef2452f6afa53f0ddc1961f97dda0bbad10e0e
unknown <ndmhaiskar@gmail.com>
2024-04-26 15:23:55 +0530

    js created practice

---
+++ index.js
@@ -0,0 +1,103 @@
+
+let arr = [1,33,2,33,34,5,6,22]
+console.log(arr)
+let mixedarr = [23,33,43,true,234.234,"nilesh"]
+console.log(mixedarr)
+mixedarr.push(100)
+console.log(mixedarr)
+mixedarr.pop()
+console.log(mixedarr)
+mixedarr.unshift(200)
+console.log(mixedarr)
+mixedarr.shift()
+console.log(mixedarr)
+console.log(mixedarr.indexOf(33))
+console.log(mixedarr.indexOf(5))
+console.log(mixedarr.length)
+/*let a1 = "name"
+let b1 = 1
+let c1 = 324
+let d1 = "10"
+let f1 = Number(d1) + b1
+let g1 = "ten"
+console.log(g1+b1)
+console.log(`${d1} plus ${b1} equal ${f1}`)
+let f2 = a1/c1
+console.log(f2)
+console.log(Number("10"))
+console.log(String(10))
+console.log(Boolean(10))
+
+
+*/
+/*
+
+
+let name4 = null
+console.log(name4)
+let name5
+console.log(name5)
+
+let x
+console.log(x)
+
+
+
+
+let n1 = 232+23
+n2 = n1*3
+n3 = n2/n1
+n4 = n2%n1
+console.log(n1,n2,n3,n4)
+console.log(n1+"Yes")
+console.log(n4+"I am here")
+console.log("n1="+n1+" n2="+n2+" n3 division:"+n3+" n4:remainder-"+n4)
+
+
+console.log(`${n1} Mul is ${n2} Div is ${n3} & Remainder is:${n4}`)
+
+let a = 10
+let b = 10.34
+console.log(a+b)
+console.log(a/b)
+
+let on = true
+let off = false
+console.log(on)
+console.log(off)
+
+
+
+
+
+
+
+let name = "Enter name"
+console.log(name)
+const  FILE_SIZE_LIMIT = 2000
+const MAX_SPEED = 100
+console.log(FILE_SIZE_LIMIT)
+console.log(MAX_SPEED)
+*/
+//if(true){
+  //  let name1 = "Gara"
+//}
+//console.log(name1)
+let name2 = "Ganesh"
+var age = 23
+let occu = "Welder"
+console.log(name2,age,occu)
+/*console.log("Hello World!--")
+Practice more
+console.log("Its a new practice")
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("Name: Yes")
+console.log("Age is: 23")
+console.log("Profession is: Welder")
+let message = "Hello World!.."
+console.log(message)
+let env = "Good"
+console.log(env)
+*/
---
+++ one.js
@@ -0,0 +1,87 @@
+flips = 0
+isHead = false
+
+while(!isHead){
+    flips++
+    isHead = Math.random() < 0.5
+}
+console.log(`Coin is flipped ${flips} times before Head comes`)
+
+
+/*
+let heads = 0
+let tails = 0
+for(let i=0;i<10;i++){
+    if(Math.random() > 0.5){
+        heads++
+    }
+    else {
+        tails++
+    }
+}
+console.log("Coin tossed 10 times")
+console.log(`heads = ${heads}`)
+console.log(`tails = ${tails}`)
+
+*/
+
+
+
+/*
+let color = ["red","blue","green"]
+for(let i=0;i<color.length;i++){
+    console.log(`${i}: ${color[i]}`)
+}
+
+for(let j=1;j<20;j+=3){
+    console.log(`j=${j}`)
+}
+
+let grade = "D"
+switch(grade){
+    case "A":
+        console.log("Chocolate")
+        break
+    case "B":
+        console.log("Coockie")
+        break
+    case "C":
+        console.log("Candy")
+        break
+    default:
+        console.log("No reward to give")
+}
+*/
+
+
+
+
+/*
+let num = 40
+switch(num){
+    case 10: 
+        console.log("num=10"+num)
+        break
+    case 20:
+        console.log(`num=20 ${num}`)
+        break
+    case 40:
+        console.log(`num=40 ${num}`)
+        break
+    default:
+        console.log( `num is available not in position`)
+
+}
+
+*/
+
+let colors = ["red","green","blue"]
+if (colors[0] == "green"){
+    console.log("Green always")
+}
+else if (colors[2] == "blue") {
+    console.log("Must be red")
+}
+else {
+    console.log("must be blue")
+}
